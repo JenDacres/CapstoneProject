7c032e748da5c45d184bfd3854992691c5667610
Kayskye <118865660+Kayskye@users.noreply.github.com>
2025-04-01 02:53:19 +0000

    modified:   Server.js

--- Server.js
+++ Server.js
@@ -53,6 +53,46 @@ app.post("/register", (req, res) => {
     });
 });
 
+// Booking Route
+app.post("/book-slot", authenticateToken, (req, res) => {
+    const { date, time_slot } = req.body;
+    const userId = req.user.userId;
+
+    db.query(
+        "INSERT INTO bookings (user_id, date, time_slot) VALUES (?, ?, ?)",
+        [userId, date, time_slot],
+        (err, result) => {
+            if (err) return res.status(500).send(err);
+            res.status(201).send("Booking successful");
+        }
+    );
+});
+
+app.get("/my-bookings", authenticateToken, (req, res) => {
+    const userId = req.user.userId;
+
+    db.query("SELECT * FROM bookings WHERE user_id = ?", [userId], (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.json(results);
+    });
+});
+
+app.delete("/cancel-booking/:id", authenticateToken, (req, res) => {
+    const bookingId = req.params.id;
+    const userId = req.user.userId;
+
+    db.query(
+        "DELETE FROM bookings WHERE id = ? AND user_id = ?",
+        [bookingId, userId],
+        (err, result) => {
+            if (err) return res.status(500).send(err);
+            res.send("Booking cancelled");
+        }
+    );
+});
+
+
+
 app.listen(5000, () => {
     console.log("Server running on port 5000...");
 });
